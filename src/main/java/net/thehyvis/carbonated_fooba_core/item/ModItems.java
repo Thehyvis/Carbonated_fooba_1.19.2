dc98d3d756fa0085ab75561e809219c73b2b41a7
Thehyvis <hvs.finska@gmail.com>
2023-08-22 18:27:19 +0300

    Modified Effetcs for Carbonated Fooba

--- src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
+++ src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, CarbonatedFoobaCore.MOD_ID);
 
     public static final RegistryObject<Item> CARBONATED_FOOBA = ITEMS.register("carbonated_fooba",
-            () -> new FoobaItem(new Item.Properties().stacksTo(7).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 15*20, 0), 0.5F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 1), 0.25F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.1F).build())));
+            () -> new FoobaItem(new Item.Properties().stacksTo(7).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 15*20, 0), 0.5F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 1), 0.1F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.05F).build())));
 
     public static final RegistryObject<Item> EMPTY_CAN = ITEMS.register("empty_can",
             () -> new Item(new Item.Properties().stacksTo(7)));
