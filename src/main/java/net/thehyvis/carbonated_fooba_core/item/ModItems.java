8229972df3cd93135295fbd400b55f36b27cd19c
Thehyvis <hvs.finska@gmail.com>
2023-08-22 19:27:31 +0300

    Added tooltips

--- src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
+++ src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
@@ -17,12 +17,12 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, CarbonatedFoobaCore.MOD_ID);
 
     public static final RegistryObject<Item> CARBONATED_FOOBA = ITEMS.register("carbonated_fooba",
-            () -> new FoobaItem(new Item.Properties().stacksTo(7).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 15*20, 0), 0.5F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 1), 0.1F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.05F).build())));
+            () -> new FoobaItem(new Item.Properties().stacksTo(17).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 10*20, 0), 0.3F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 2), 0.05F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.05F).build())));
 
     public static final RegistryObject<Item> EMPTY_CAN = ITEMS.register("empty_can",
-            () -> new Item(new Item.Properties().stacksTo(7)));
+            () -> new Item(new Item.Properties().stacksTo(17)));
     public static final RegistryObject<Item> TIN_CAN = ITEMS.register("tin_can",
-            () -> new Item(new Item.Properties().stacksTo(7)));
+            () -> new Item(new Item.Properties().stacksTo(17)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
--- src/main/java/net/thehyvis/carbonated_fooba_core/item/custom/FoobaItem.java
+++ src/main/java/net/thehyvis/carbonated_fooba_core/item/custom/FoobaItem.java
@@ -1,5 +1,7 @@
 package net.thehyvis.carbonated_fooba_core.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -7,9 +9,25 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.thehyvis.carbonated_fooba_core.item.ModItems;
-
+import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
+import java.awt.*;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
 public class FoobaItem extends Item {
     public FoobaItem(Properties properties) {
         super(properties);
@@ -29,6 +47,18 @@ public class FoobaItem extends Item {
             }
             return itemstack;
         }
+
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()) {
+        components.add(Component.translatable("tooltip.carbonated_fooba_core.carbonated_fooba_shift").withStyle(ChatFormatting.WHITE));
+        } else {
+            components.add(Component.translatable("tooltip.carbonated_fooba_core.carbonated_fooba").withStyle(ChatFormatting.AQUA));
+        }
+
+        super.appendHoverText(stack, level, components, flag);
 
     }
 }
