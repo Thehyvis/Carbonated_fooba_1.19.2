afd411d134745932bc172ac3084b9c69b910b78d
Thehyvis <hvs.finska@gmail.com>
2023-08-22 22:42:03 +0300

    Added water bottles

--- src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
+++ src/main/java/net/thehyvis/carbonated_fooba_core/item/ModItems.java
@@ -3,6 +3,7 @@ import com.mojang.blaze3d.shaders.Effect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
 import net.thehyvis.carbonated_fooba_core.CarbonatedFoobaCore;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,12 +18,19 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, CarbonatedFoobaCore.MOD_ID);
 
     public static final RegistryObject<Item> CARBONATED_FOOBA = ITEMS.register("carbonated_fooba",
-            () -> new FoobaItem(new Item.Properties().stacksTo(17).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 10*20, 0), 0.3F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 2), 0.05F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.05F).build())));
+            () -> new FoobaItem(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(17).food(new FoodProperties.Builder().nutrition(4).saturationMod(4).alwaysEat().effect(() -> new MobEffectInstance(MobEffects.HEALTH_BOOST, 2000, 2), 1F).effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 10*20, 0), 0.3F).effect(() -> new MobEffectInstance(MobEffects.HEAL, 1, 4), 1F).effect(() -> new MobEffectInstance(MobEffects.POISON, 100, 2), 0.05F).effect(() -> new MobEffectInstance(MobEffects.WITHER, 60, 1), 0.05F).build())));
 
     public static final RegistryObject<Item> EMPTY_CAN = ITEMS.register("empty_can",
-            () -> new Item(new Item.Properties().stacksTo(17)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(17)));
     public static final RegistryObject<Item> TIN_CAN = ITEMS.register("tin_can",
-            () -> new Item(new Item.Properties().stacksTo(17)));
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(17)));
+
+
+    public static final RegistryObject<Item> ENLARGED_GLASS_BOTTLE = ITEMS.register("enlarged_glass_bottle",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(17)));
+
+    public static final RegistryObject<Item> ENLARGED_WATER_BOTTLE = ITEMS.register("enlarged_water_bottle",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).stacksTo(17)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
